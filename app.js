3db76b110886674cc4e8a3a2f01718e44c8d95ab
Mohammad Akbarzadeh <mohammadakzadeh@gmail.com>
2021-04-27 12:21:46 -0400

    mobile friendly

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let controller;
 let slideScene;
 let pageScene;
+let detailScene;
 
 function animateSlides() {
   //Init Controller
@@ -103,6 +104,7 @@ function navToggle(e) {
 }
 
 //barba page transition
+const logo = document.querySelector("#logo");
 barba.init({
   views: [
     {
@@ -133,32 +135,71 @@ barba.init({
     {
       leave({ current, next }) {
         let done = this.async();
-        //Scroll to the top
-        window.scrollTo(0, 0);
-        //Animation
+        //An Animation
         const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
         tl.fromTo(
-          current.container,
-          1,
-          { opacity: 1 },
-          { opacity: 0, onComplete: done }
+          ".swipe",
+          0.75,
+          { x: "-100%" },
+          { x: "0%", onComplete: done },
+          "-=0.5"
         );
       },
       enter({ current, next }) {
         let done = this.async();
-
-        //Animation
+        //Scroll to the top
+        window.scrollTo(0, 0);
+        //An Animation
         const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
         tl.fromTo(
-          next.container,
+          ".swipe",
           1,
-          { opacity: 0 },
-          { opacity: 1, onComplete: done }
+          { x: "0%" },
+
+          { x: "100%", stagger: 0.2, onComplete: done }
+        );
+        tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+        tl.fromTo(
+          ".nav-header",
+          1,
+          { y: "-100%" },
+          { y: "0%", ease: "power2.inOut" },
+          "-=1.5"
         );
       },
     },
   ],
 });
+
+//Fashion page
+
+function detailAnimation() {
+  controller = new ScrollMagic.Controller();
+  const slides = document.querySelectorAll(".detail-slide");
+  slides.forEach((slide, index, slides) => {
+    const slideTl = gsap.timeline({ defaults: { duration: 1 } });
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    const nextImg = nextSlide.querySelector("img");
+    slideTl.fromTo(slide, { opacity: 1 }, { opacity: 0 });
+    slideTl.fromTo(nextSlide, { opacity: 0 }, { opacity: 1 }, "-=1");
+    slideTl.fromTo(nextImg, { x: "50%" }, { x: "0%" });
+    //Scene
+    detailScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(slideTl)
+      // .addIndicators({
+      //   colorStart: "white",
+      //   colorTrigger: "white",
+      //   name: "detailScene"
+      // })
+      .addTo(controller);
+  });
+}
 
 //Event listerners
 burger.addEventListener("click", navToggle);
